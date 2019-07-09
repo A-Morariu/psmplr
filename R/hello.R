8b4e564ff4c090ed33be663aa01a17b464286715
Alin Morariu <amorariu06@gmail.com>
2019-07-09 09:31:51 -0400

    First commit
    Initialize the project, code to come

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+# psmplr package
+#
+# Posterior distribution sampling framework created to take in an INLA model
+# and output a sample to mimic all configurations of the model
+#
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
